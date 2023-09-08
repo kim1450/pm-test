05b88780b732e496d56036a06f192d60f3082df4
SS0511 <SS0511@DESKTOP-S0UFN1I>
2023-09-08 19:11:45 +0900

    .

---
+++ Hello/src/hello.java
@@ -0,0 +1,9 @@
+
+public class hello {
+	public static void main(String[] args) {
+		System.out.println("hello world!");
+		System.out.print("hello, world!"); // 이거는 줄넘김 없음
+		System.out.print("\n");
+		System.out.println("difference between println and print");
+	}
+}
