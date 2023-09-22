f4a8ed5f66919cd24e2e1620b9a377d6d1d60757
kim1450 <139091766+kim1450@users.noreply.github.com>
2023-09-22 13:04:40 +0900

    23.09.22 PM 1:04

--- Hello/src/Homework3.java
+++ Hello/src/Homework3.java
@@ -6,21 +6,21 @@ public class Homework3 {
 		Scanner scanner = new Scanner(System.in);
 		System.out.println("input price(won) : ");
 		int sale = scanner.nextInt();
-		int[] a = {50000,10000,5000,1000,500,100,50,10,1};
-		int[] b = {0,0,0,0,0,0,0,0,0}; 
-		for(int i=0;i < a.length;i++) {
-			if (sale/a[i] != 0) {
-				b[i] = sale / a[i];  
-				sale = sale % a[i];
+		int[] unit = {50000,10000,5000,1000,500,100,50,10,1};
+		int[] result = {0,0,0,0,0,0,0,0,0}; 
+		for(int i=0;i < unit.length;i++) {
+			if (sale/unit[i] != 0) {
+				result[i] = sale / unit[i];  
+				sale = sale % unit[i];
 //				System.out.println(sale);
 			}else {
-				b[i] = 0;
+				result[i] = 0;
 			}
 		}
 		
-		for(int i=0;i < a.length;i++) {
-			if (b[i] != 0) {
-				System.out.printf("%d: %d %n",a[i],b[i]);
+		for(int i=0;i < unit.length;i++) {
+			if (result[i] != 0) {
+				System.out.printf("%d: %d %n",unit[i],result[i]);
 			}else {
 				continue;
 			}
